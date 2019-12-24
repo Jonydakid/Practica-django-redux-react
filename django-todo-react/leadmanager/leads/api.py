c4da56bcbc283a9e19d229ca5c0f62a27c2f50d3
Jonydakid <jonathan.torres.salvo@gmail.com>
2019-12-24 11:07:56 -0300

    autenticación con token
    mensajes de error en alerts

--- django-todo-react/leadmanager/leads/api.py
+++ django-todo-react/leadmanager/leads/api.py
@@ -1,12 +1,19 @@
-from leads.models  import Lead
+from leads.models import Lead
 from rest_framework import viewsets, permissions
 from .serializers import LeadSerializer
 
-#Lead ViewSet--> El viewset permite crear una API CRUD sin especificar metodos explicitos.
-class LeadViewset(viewsets.ModelViewSet):
-    queryset=Lead.objects.all()
-    permission_classes=[
-        permissions.AllowAny
-    ]
+# Lead ViewSet--> El viewset permite crear una API CRUD sin especificar metodos explicitos.
 
-    serializer_class=LeadSerializer
+
+class LeadViewset(viewsets.ModelViewSet):
+
+    permission_classes = [
+        permissions.IsAuthenticated
+    ]
+    serializer_class = LeadSerializer
+
+    def get_queryset(self):
+        return self.request.user.leads.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
